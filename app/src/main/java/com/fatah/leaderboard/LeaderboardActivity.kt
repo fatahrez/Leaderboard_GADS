6f38c728a1cdf20d0ef3b76fffcf679cba811bad
fatahrez <fertahrez@gmail.com>
2020-08-30 01:06:48 +0300

    Splash Screen implemented move to next task

---
+++ app/src/main/java/com/fatah/leaderboard/LeaderboardActivity.kt
@@ -0,0 +1,11 @@
+package com.fatah.leaderboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeaderboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leaderboard)
+    }
+}
--- app/src/main/java/com/fatah/leaderboard/MainActivity.kt
+++ app/src/main/java/com/fatah/leaderboard/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.fatah.leaderboard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT: Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, LeaderboardActivity::class.java))
+
+            finish()
+        }, SPLASH_TIME_OUT)
     }
 }
